8fa6b621bb8160d1637fc4f26787a5af046f7f67
phannam1412 <phannam1412@yahoo.com>
2022-07-07 08:24:02 +0700

    Update printer.go

--- printer.go
+++ printer.go
@@ -1,16 +1,23 @@
 package printer
 
-func FullLine(text string) error {
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func FullLine(text string) {
 	fmt.Print(text)
 	w, _, err := terminal.GetSize(0)
 	if err != nil {
-		return err
+		fmt.Println(text)
+		return
 	}
-	more := w - len(text) - 2
-	if more <= 1 {
-		return nil
+	more := w - len(text) - 1
+	if more <= 2 {
+		fmt.Println(text)
+		return
 	}
-	fmt.Print(" " + strings.Repeat("*", more))
-	fmt.Println()
-	return nil
+	fmt.Print(" " + strings.Repeat("*", more) + "\n")
 }
